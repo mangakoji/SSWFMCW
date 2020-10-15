38a52179d8907d40a970c045f6ba1f8b716d86f6
mangakoji <shibaoka.shuuichi@gmail.com>
2020-10-15 17:32:58 +0900

    done compile SSFMCW , WO sim

--- RTL/MAIN/SSWFMCW.v
+++ RTL/MAIN/SSWFMCW.v
@@ -15,10 +15,10 @@ module SSWFMCW
       `in tri0      CK_i          //48MHz
     , `in tri1      XARST_i
     , `in tri1      CK_EE_i
-    , `out `w       TXSP_o
-    , `out `w       MIC_CK_o
-    , `in tri0[1:0] MIC_DATs_i
-    ,  `out `w[1:0] HEAD_PHONEs_o ;
+    , `out`w       TXSP_o
+    , `out`w       MIC_CK_o
+    , `in tri0[1:0] MICs_DAT_i
+    , `out`w[1:0] HEAD_PHONEs_o 
 ) ;
     `func `int log2 ;
         `in `int value ;
@@ -39,7 +39,7 @@ module SSWFMCW
             DN_XUP <= 1'b0 ;
         `e else
         `b  if(~DN_XUP)
-            `b  if(ADD_Ds >= {ADD_MAX,12'h0})
+            `b  if(ADD_Ds >= {C_ADD_MAX,12'h0})
                 `b  DN_XUP <= 1'b1 ;
                     ADD_Ds <= ADD_Ds -1 ;
                 `e else
@@ -60,9 +60,9 @@ module SSWFMCW
         else
             WAVE_CTRs <= WAVE_CTRs + ADD_Ds[25:12] ; //ADD_D14bit
     // sin wave gen
-    'w `s[11:0] COS_WAVEs ;
-    'w `s[11:0] SIN_WAVEs ;
-    'w `s[11:0] cos_ctr_s = 
+    `w`s[11:0] COS_WAVEs ;
+    `w`s[11:0] SIN_WAVEs ;
+    `w`s[11:0] cos_ctr_s = 
                     { 
                           ~( ^ WAVE_CTRs[23:22])
                         , ~    WAVE_CTRs[22]
@@ -75,7 +75,7 @@ module SSWFMCW
               .CK_i         ( CK_i              )
             , .XARST_i      ( XARST_i           )
             , .DAT_i        ( cos_ctr_s         )//2's -h800 0 +7FFF
-            , .SIN_o        ( SIN_WAVEs         )//2's -h800 0 +h800
+            , .SIN_o        ( COS_WAVEs         )//2's -h800 0 +h800
         )
     ;
     SIN_TBL_s11_s11 
@@ -97,7 +97,7 @@ module SSWFMCW
                   {1'b0 ,TXSP_DSs[11:0]} 
                 + {~COS_WAVEs[11],COS_WAVEs[10:0]} 
             ;
-    `a TXSP_o = TX_Dss[12] ;
+    `a TXSP_o = TXSP_DSs[12] ;
     
     // MIC_CLOCK
     `r[2:0]MIC_CTRs ;
@@ -109,11 +109,11 @@ module SSWFMCW
             MIC_CK <= 1'b0 ;
             MIC_EE <= 1'b0 ;
         `e else
-        `b  if(&(MIC_CTRs|~(3'd5))
+        `b  if(&(MIC_CTRs|~(3'd5)))
             `b  MIC_CTRs <= 0;
                 MIC_CK <= ~ MIC_CK ;
                 MIC_EE <= MIC_CK ;
-            `e else 
+            `e else
             `b  MIC_CTRs <= MIC_CTRs + 1 ;
                 MIC_EE <= 1'b0 ;
             `e
@@ -121,69 +121,76 @@ module SSWFMCW
     `a MIC_CK_o = MIC_CK ;
     // HeadPhone DS
     `gen
-        genvar gi l
+        genvar gi ;
         for(gi=0;gi<2;gi=gi+1)
-        `b
+        `b :g_PHONE
             // MIC IF
-            `r[1:0] MIC_Ds
+            `r[1:0] MIC_Ds ;
             `ack
                 `xar
                     MIC_Ds <= 0 ;
                 else
                     if(MIC_EE)
-                        MIC_Ds <= {MIC_Ds,MICs_i[gi] ;
+                        MIC_Ds <= {MIC_Ds,MICs_DAT_i[gi]} ;
             `r`s[24:0]IIRs_COS ;
             `r`s[24:0]IIRs_SIN ;
             // MIC_IIR // for test
-                    if
-                    (MIC_Ds[1])
             `w`s[25:0] COS_diff_s =
                     (MIC_Ds[1])
-                    ?   ($`s(IIRs_COS[24:13]) + $`s(COS_WAVEs))
-                    :   ($`s(IIRs_COS[24:13]) - $`s(COS_WAVEs)) 
+                    ?   (`Ds(IIRs_COS[24:13]) + `Ds(COS_WAVEs))
+                    :   (`Ds(IIRs_COS[24:13]) - `Ds(COS_WAVEs)) 
             ;
             `w`s[25:0] SIN_diff_s =
                     (MIC_Ds[1])
-                    ?   ($`s(IIRs_SIN[24:13]) + $`s(SIN_WAVEs))
-                    :   ($`s(IIRs_SIN[24:13]) - $`s(SIN_WAVEs)) 
+                    ?   (`Ds(IIRs_SIN[24:13]) + `Ds(SIN_WAVEs))
+                    :   (`Ds(IIRs_SIN[24:13]) - `Ds(SIN_WAVEs)) 
             ;
+            `r`s[12:0] COS_DIFFs ;
+            `r`s[12:0] SIN_DIFFs ;
             // fc=983Hz = 48MHz/((2**13)*2*pi)
             `ack
                 `xar
-                `b      IIRs_COS <= 1<<24 ;
-                        IIRs_SIN <= 1<<24 ;
-                `e 
-                `b
-                        IIRs_COS <= $`s(IIRs_COS) + $`s(COS_diff_s[25:13]) ;
-                        IIRs_SIN <= $`s(IIRs_SIN) + $`s(SIN_diff_s[25:13]) ;
+                `b  COS_DIFFs <= 0 ;
+                    SIN_DIFFs <= 0 ;
+                    IIRs_COS <= 0 ;
+                    IIRs_SIN <= 0 ;
+                `e else
+                `b  COS_DIFFs <= COS_diff_s[25:13] ;
+                    SIN_DIFFs <= SIN_diff_s[25:13] ;
+                    IIRs_COS <= `Ds(IIRs_COS) + `Ds(COS_DIFFs) ;
+                    IIRs_SIN <= `Ds(IIRs_SIN) + `Ds(SIN_DIFFs) ;
                 `e
-            `w[21:0] COS_squ_s = $`s(IIRs_COS[24:13]) * $`s(IIRs_COS[24:13]) ;
-            `w[21:0] SIN_squ_s = $`s(IIRs_SIN[24:13]) * $`s(IIRs_SIN[24:13]) ;
+            `w[21:0] COS_squ_s = `Ds(IIRs_COS[24:13]) * `Ds(IIRs_COS[24:13]) ;
+            `r[21:0] COS_SQUs ;
+            `w[21:0] SIN_squ_s = `Ds(IIRs_SIN[24:13]) * `Ds(IIRs_SIN[24:13]) ;
+            `r[21:0] SIN_SQUs ;
+            `r[22:0] SQUs ;
             `ack
                 `xar
-                `b
+                `b  COS_SQUs <= 0 ;
+                    SIN_SQUs <= 0 ;
+                    SQUs    <= 0 ;
                 `e else
-                `b 
-                    COS_SQU <= COS_squ_s[21:0] ;             // /22
-                    SIN_SQU <= SIN_squ_s[21:0] ;             // /22
-                    SQUs <= {1'b0,SIN_SQU} + {1'b0,COS_SQU} ;// /23
+                `b  COS_SQUs <= COS_squ_s[21:0] ;             // /22
+                    SIN_SQUs <= SIN_squ_s[21:0] ;             // /22
+                    SQUs <= {1'b0,SIN_SQUs} + {1'b0,COS_SQUs} ;// /23
                 `e
+            `w[11:0] RMSs ;
             SQRT
                 SQRT
-                (
-                      .CK_i     ( CK_i      )
+                (     .CK_i     ( CK_i      )
                     , .XARST_i  ( XARST_i   )
                     , .DATs_i   ( SQUs      )
                     , .QQs_o    ( RMSs      ) //12
                 ) 
             ;
-            `r[12:0] DSs ;
+            `r[12:0] EP_DSs ;
             `ack
                 `xar
-                    DSs <= 13'b1_1000_0000_0000 ;
+                    EP_DSs <= 13'b1_1000_0000_0000 ;
                 else
-                    DSs <= {1'b0 ,DSs[11:0]} + {RMSs[10:0]} ;
-            `a HEAD_PHONEs_o[gi] = PHONEs[10] ;
+                    EP_DSs <= {1'b0 ,EP_DSs[11:0]} + {RMSs[10:0]} ;
+            `a HEAD_PHONEs_o[gi] = EP_DSs[10] ;
         `e
     `egen
 endmodule
